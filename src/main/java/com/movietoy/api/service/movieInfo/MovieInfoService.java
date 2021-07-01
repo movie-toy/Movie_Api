bd6a5c75727e5b437f99420ce2b64fbe4dedd4db
Sangin-C <sangin3435@naver.com>
2021-07-01 23:43:02 +0900

    MovieInfo Cacheable 적용

--- src/main/java/com/movietoy/api/service/movieInfo/MovieInfoService.java
+++ src/main/java/com/movietoy/api/service/movieInfo/MovieInfoService.java
@@ -3,6 +3,7 @@ package com.movietoy.api.service.movieInfo;
 import com.movietoy.api.domain.movieInfo.MovieInfo;
 import com.movietoy.api.domain.movieInfo.MovieInfoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +16,7 @@ public class MovieInfoService {
 
     //영화 상세 정보 조회
     @Transactional(readOnly = true)
+    @Cacheable("MovieInfo")
     public MovieInfo MovieInfo(String movieCd) {
         return movieInfoRepository.findByMovieCd(movieCd);
     }
